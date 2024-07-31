00eb9f9ee7ea2f1a5feb44df7c30ad4a0c943cea
泛红橘子 <bingxio@qq.com>
2024-07-31 14:18:49 +0800

    update.

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"log"
 	"sy_backend/config"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -18,7 +19,35 @@ func Open() error {
 		return err
 	}
 	Conn = db
+	makeTable()
 	return nil
+}
+
+func makeTable() {
+	rows, err := Conn.Query("PRAGMA table_info(menu)")
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+	exists := rows.Next()
+
+	if !exists {
+		_, err = Conn.Exec(`CREATE TABLE "menu" (
+	"_id"	INTEGER NOT NULL UNIQUE,
+	"title"	TEXT,
+	"type"	TEXT,
+	"ingredients"	TEXT,
+	"cook_method"	TEXT,
+	"image_list"	TEXT,
+	"budget"	REAL,
+	"create_at"	TEXT DEFAULT (datetime('now', 'localtime')),
+	"update_at"	TEXT DEFAULT (datetime('now', 'localtime')),
+	PRIMARY KEY("_id" AUTOINCREMENT)
+);`)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
 
 func Close() error {
--- go.mod
+++ go.mod
@@ -4,7 +4,4 @@ go 1.22.5
 
 require github.com/bingxio/dotenv v1.0.1
 
-require (
-	github.com/mattn/go-sqlite3 v1.14.22 // indirect
-	golang.org/x/crypto v0.25.0
-)
+require github.com/mattn/go-sqlite3 v1.14.22

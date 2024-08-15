fd0d3af9cdd970dfdef22cea31d424f3c89a9c94
泛红橘子 <bingxio@qq.com>
2024-08-15 15:31:53 +0800

    Remove tags.

--- api/menu.go
+++ api/menu.go
@@ -14,9 +14,8 @@ import (
 
 type MenuModel struct {
 	Id          string    `json:"_id"`
-	Title       string    `json:"title"` // 名称
-	Type        string    `json:"type"`  // 荤、素、汤
-	Tag         []string  `json:"tag"`
+	Title       string    `json:"title"`       // 名称
+	Type        string    `json:"type"`        // 荤、素、汤
 	Ingredients []string  `json:"ingredients"` // 食材
 	CookMethod  string    `json:"cook_method"` // 烹饪方法
 	ImagePath   string    `json:"image_path"`  // 样例图
@@ -51,19 +50,17 @@ func GetMenuList(w http.ResponseWriter, r *http.Request) {
 
 	for rows.Next() {
 		var menu MenuModel
-		var tag, ingre, modify string
+		var ingre, modify string
 
 		err := rows.Scan(
-			&menu.Id, &menu.Title, &menu.Type, &tag, &ingre, &menu.CookMethod,
+			&menu.Id, &menu.Title, &menu.Type, &ingre, &menu.CookMethod,
 			&menu.ImagePath, &modify,
 		)
 		if err != nil {
 			log.Println(err)
 			return
 		}
-		menu.Tag = strings.Split(tag, ",")
 		menu.Ingredients = strings.Split(ingre, ",")
-
 		menu.ModifyAt, _ = time.Parse("2006-01-02 15:04:05", modify)
 
 		menuList = append(menuList, menu)
@@ -74,7 +71,6 @@ func GetMenuList(w http.ResponseWriter, r *http.Request) {
 func PostMenu(w http.ResponseWriter, r *http.Request) {
 	title := r.FormValue("title")
 	tp := r.FormValue("type")
-	tag := r.FormValue("tag")
 	ingredients := r.FormValue("ingredients") // 食材
 	cookMethod := r.FormValue("cook_method")  // 烹饪方法
 	files := r.MultipartForm.File["image"]
@@ -88,12 +84,11 @@ func PostMenu(w http.ResponseWriter, r *http.Request) {
 	}
 	_, err = db.Conn.Exec(
 		`INSERT INTO menu(
-			_id, title, type, tag, ingredients, cook_method, image_path)
-		VALUES(?, ?, ?, ?, ?, ?, ?)`,
+			_id, title, type, ingredients, cook_method, image_path)
+		VALUES(?, ?, ?, ?, ?, ?)`,
 		id,
 		title,
 		tp,
-		tag,
 		ingredients,
 		cookMethod,
 		path,
@@ -138,8 +133,6 @@ func PatchMenu(w http.ResponseWriter, r *http.Request) {
 		value = r.FormValue("cook_method")
 	case "type":
 		value = r.FormValue("type")
-	case "tag":
-		value = r.FormValue("tag")
 	}
 	if field == "cook-method" {
 		field = "cook_method"
